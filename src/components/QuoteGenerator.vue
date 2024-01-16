267cd829135ddaf99448289da4df8ae1b8308bc8
Camilla Westin <camilla@webstin.se>
2024-01-16 15:51:45 +0100

    Quote generator component

---
+++ src/components/QuoteGenerator.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const quotes = ref([]);
+
+onMounted(async () => {
+  try {
+    const category = "happiness";
+    const apiKey = import.meta.env.VITE_NINJA_API_KEY;
+
+    const response = await fetch(
+      `https://api.api-ninjas.com/v1/quotes?category=${category}`,
+      {
+        method: "GET",
+        headers: {
+          "X-Api-Key": apiKey,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      quotes.value = data;
+    } else {
+      console.error("Error fetching data:", response.statusText);
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+});
+</script>
+
+<template>
+  <div class="flex items-center justify-center p-6">
+    <div class="text-center bg-pink-300 p-6">
+      <h2 class="text-center font-semibold pb-4">Quote of the day</h2>
+      <div v-for="quote in quotes" :key="quote.id">
+        <span class="font-bold text-lg md:text-xl italic block"
+          >"{{ quote.quote }}"</span
+        >
+        <span class="pt-3 text-sm block">- {{ quote.author }}</span>
+      </div>
+    </div>
+  </div>
+</template>
