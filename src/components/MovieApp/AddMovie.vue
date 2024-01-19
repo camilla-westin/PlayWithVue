1bce71ecdaad12df5d9235924275f272b108b0a5
Camilla Westin <camilla@webstin.se>
2024-01-19 16:08:35 +0100

    Added transition to form modal

--- src/components/MovieApp/AddMovie.vue
+++ src/components/MovieApp/AddMovie.vue
@@ -88,91 +88,93 @@ const clearForm = () => {
 </script>
 
 <template>
-  <div
-    class="modal fixed top-0 left-0 h-screen w-screen z-10 flex justify-center items-center"
-  >
-    <div class="w-1/2 bg-vue-dark-grey z-20 p-6">
-      <h2 class="text-2xl text-white font-semibold">Add movie</h2>
-      <form id="movieform" @submit.prevent="onSubmit">
-        <div>
-          <label for="name">Name</label>
-          <input
-            id="name"
-            v-model="movieName"
-            placeholder="Movie name.."
-            type="text"
-          />
-        </div>
-        <div>
-          <label for="description">Description</label>
-          <textarea
-            id="description"
-            v-model="movieDescription"
-            placeholder="Description.."
-          />
-        </div>
-        <div>
-          <label for="image">Image url</label>
-          <input
-            id="image"
-            v-model="movieImg"
-            placeholder="Url to image.."
-            type="text"
-          />
-        </div>
-
-        <div>
-          <label for="genres">Genres</label>
-          <select v-model="movieGenres" id="genres" multiple>
-            <option
-              v-for="option in movieGenresOptions"
-              :key="option.value"
-              :value="option.value"
-            >
-              {{ option.value }}
-            </option>
-          </select>
-        </div>
-
-        <div>
-          <legend>In theathers</legend>
-          <div class="flex gap-2">
+  <Transition name="fade" :duration="500">
+    <div
+      class="modal fixed top-0 left-0 h-screen w-screen z-10 flex justify-center items-center"
+    >
+      <div class="form w-1/2 bg-vue-dark-grey z-20 p-6">
+        <h2 class="text-2xl text-white font-semibold">Add movie</h2>
+        <form id="movieform" @submit.prevent="onSubmit">
+          <div>
+            <label for="name">Name</label>
             <input
-              type="radio"
-              id="yes"
-              value="true"
-              v-model="movieInTheathers"
+              id="name"
+              v-model="movieName"
+              placeholder="Movie name.."
+              type="text"
             />
-            <label for="yes">Yes</label>
-
-            <input
-              type="radio"
-              id="no"
-              value="false"
-              v-model="movieInTheathers"
-            />
-            <label for="no">No</label>
           </div>
-        </div>
-        <div class="flex justify-between flex-wrap">
-          <button
-            type="button"
-            @click="clearForm"
-            class="bg-slate-200 py-1 px-3 font-medium my-4 rounded"
-          >
-            Cancel
-          </button>
-          <button
-            type="submit"
-            class="bg-vue-green py-1 px-3 text-white font-medium my-4 rounded"
-          >
-            <span v-if="editMode">Update</span>
-            <span v-else>+ Add</span>
-          </button>
-        </div>
-      </form>
+          <div>
+            <label for="description">Description</label>
+            <textarea
+              id="description"
+              v-model="movieDescription"
+              placeholder="Description.."
+            />
+          </div>
+          <div>
+            <label for="image">Image url</label>
+            <input
+              id="image"
+              v-model="movieImg"
+              placeholder="Url to image.."
+              type="text"
+            />
+          </div>
+
+          <div>
+            <label for="genres">Genres</label>
+            <select v-model="movieGenres" id="genres" multiple>
+              <option
+                v-for="option in movieGenresOptions"
+                :key="option.value"
+                :value="option.value"
+              >
+                {{ option.value }}
+              </option>
+            </select>
+          </div>
+
+          <div>
+            <legend>In theathers</legend>
+            <div class="flex gap-2">
+              <input
+                type="radio"
+                id="yes"
+                value="true"
+                v-model="movieInTheathers"
+              />
+              <label for="yes">Yes</label>
+
+              <input
+                type="radio"
+                id="no"
+                value="false"
+                v-model="movieInTheathers"
+              />
+              <label for="no">No</label>
+            </div>
+          </div>
+          <div class="flex justify-between flex-wrap">
+            <button
+              type="button"
+              @click="clearForm"
+              class="bg-slate-200 py-1 px-3 font-medium my-4 rounded"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              class="bg-vue-green py-1 px-3 text-white font-medium my-4 rounded"
+            >
+              <span v-if="editMode">Update</span>
+              <span v-else>+ Add</span>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <style scoped>
@@ -202,5 +204,25 @@ select {
 
 .modal {
   background-color: rgba(0, 0, 0, 0.8);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active .form,
+.fade-leave-active .form {
+  transition: transform 0.6s ease;
+}
+
+.fade-enter-from .form,
+.fade-leave-to .form {
+  transform: translateY(50px);
 }
 </style>
